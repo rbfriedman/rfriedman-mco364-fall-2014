3294e633ea3deaa210182c5fb0f841b4aa901988
rbfriedman <renabf@gmail.com>
2014-09-16 22:34:48 -0400

    Fixed bug in readLine loop and added a counter

--- src/friedman/network/ResponseServer.java
+++ src/friedman/network/ResponseServer.java
@@ -9,25 +9,29 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ResponseServer {
-	public static void main(String args[]) throws IOException{
+	public static void main(String args[]) throws IOException {
 		ServerSocket serverSocket = new ServerSocket(8080);
 		Socket socket = serverSocket.accept();
-		
+
 		InputStream in = socket.getInputStream();
-		
+
 		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-		
-		String line ="";
-		StringBuilder response =new StringBuilder();
-		while((line = reader.readLine())!=null){
-			response.append(line);
+
+		String line = "";
+		int counter = 0;
+		while (true) {
+			while (!"".equals((line = reader.readLine()) != null)) {
+				System.out.println(line);
+			}
+
+			OutputStream out = socket.getOutputStream();
+			String response = "<h5>This is the " + ++counter + "st request</h2>";
+			out.write(response.getBytes());
+			out.write("HTTP/1.1 200 OK\n".getBytes());
+			out.write("Content-Type: text/html); charset=utf-8\n".getBytes());
+			out.write(("Content-Length: " + response.length() + " ").getBytes());
+			out.flush();
+			out.close();
 		}
-		System.out.println(response.toString());
-		OutputStream out = socket.getOutputStream();
-		
-		out.write("Hello World".getBytes());
-		
-		out.flush();
-		out.close();
 	}
 }
