5d8b26d0dda15fa7093f00770e0a940ba276d32e
rbfriedman <renabf@gmail.cmom>
2014-12-19 14:46:40 -0500

    put Layer into canvas

--- src/friedman/paint/PaintBanner.java
+++ src/friedman/paint/PaintBanner.java
@@ -3,27 +3,15 @@ package friedman.paint;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics2D;
 import java.awt.GridLayout;
-import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseMotionListener;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-
 import javax.swing.AbstractButton;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JColorChooser;
-import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
 import javax.swing.border.EtchedBorder;
-import javax.swing.event.ChangeEvent;
-import javax.swing.event.ChangeListener;
 
 import friedman.paint.listeners.BucketFillListener;
 import friedman.paint.listeners.ClearScreenListener;
