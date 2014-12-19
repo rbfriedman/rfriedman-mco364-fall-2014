e88304fdcd451b0807f0e4a46ed6cf16446142c0
rbfriedman <renabf@gmail.cmom>
2014-12-19 13:15:51 -0500

    MouseDragged implemented in FillRectListener
    preview was not showing

--- src/friedman/paint/drawing/FillRectangleListener.java
+++ src/friedman/paint/drawing/FillRectangleListener.java
@@ -16,6 +16,28 @@ public class FillRectangleListener extends DrawShapeListener {
 
 
 	@Override
+	public void mouseDragged(MouseEvent me) {
+		// TODO Auto-generated method stub
+		super.mouseDragged(me);
+		canvas.repaint();
+	}
+
+
+	@Override
+	public void mousePressed(MouseEvent me) {
+		// TODO Auto-generated method stub
+		super.mousePressed(me);
+	}
+
+
+	@Override
+	public void mouseMoved(MouseEvent me) {
+		// TODO Auto-generated method stub
+		super.mouseMoved(me);
+	}
+
+
+	@Override
 	public void mouseReleased(MouseEvent me) {
 		super.mouseReleased(me);
 		Graphics2D g2 = (Graphics2D) canvas.getImage().getGraphics();
