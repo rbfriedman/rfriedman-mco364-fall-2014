c5064d6c305b7039c4085391fa0d7dfcbaf78409
rbfriedman <renabf@gmail.cmom>
2014-12-30 23:13:08 -0500

    Revert "j"
    This reverts commit 674cd4c211891ac475a083fe16d0a402baa4a9fc.

--- src/friedman/paint/OnlineNetworkModule.java
+++ src/friedman/paint/OnlineNetworkModule.java
@@ -1,22 +1,13 @@
 package friedman.paint;
 
-import java.io.PrintWriter;
-
 import friedman.paint.messages.PaintMessage;
 
 public class OnlineNetworkModule implements NetworkModule {
-	private Canvas canvas;
-	public OnlineNetworkModule(Canvas canvas){
-		this.canvas = canvas;
-	}
+
 	@Override
 	public void sendMessage(PaintMessage pm) {
 		// TODO Auto-generated method stub
-		LOGGER.info(pm.toString());
-		String message = pm.toString();
-		PrintWriter writer = canvas.getPrintWriter();
-		writer.print(message);
-		writer.flush();
+		
 	}
 
 }
